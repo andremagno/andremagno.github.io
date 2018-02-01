a31ef50060a9e6b3bf7a6ee520ec4f6803e3c97b
André Magno <andremagno@live.com>
2018-02-01 12:41:43 -0200

    Trabalhos finalizados

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,27 +10,28 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-require('./lib/jquery.visible.min.js');
 
 $(document).foundation();
 
 $(document).ready(function(){
-	
-	$(".horizontal").on( 'scroll', function(){
-		var i=0;
-		$('.horizontal .project__link').each(function(){	
-			// Is this element visible onscreen?
-			var visible = $(this).visible();
-			
-			// Set the visible status into the span.
-			$(this).toggleClass('active',visible);			
-
-			if ($(this).is(':first-child')) {
-		        i=0;
-		    }
-		    i++;
-			$('.graph .bar:nth-child('+i+')').toggleClass('active',visible);
-		});
+	$('a.work__reveal').click(function(){
+		var url = $(this).find('img')[0].src;
+		var img = $(this).html();
+		if (Foundation.MediaQuery.atLeast('medium')) {
+			$('#element').foundation('open');
+			$('.work__modal-img').html(img);		 
+		} else {
+			window.open(url,'_blank','',''); 
+		}		
 	});
-
+	$('ul.work__list li').each(function(){
+		// Get the content
+		var str = $(this).html();
+		// Set the regex string
+		var regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/ig
+		// Replace plain text links by hyperlinks
+		var replaced_text = str.replace(regex, "<a href='$1' target='_blank'>$1</a>");
+		// Echo link
+		$(this).html(replaced_text);
+	});
 });
