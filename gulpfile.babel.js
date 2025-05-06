03c464e6f258afe984f819f31866308f1775cb63
André Magno <andrmgno@gmail.com>
2025-05-06 18:20:03 -0300

    Migração para o pacote mais atualizado do Foundation (6.9.0)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,23 @@
-import plugins       from 'gulp-load-plugins';
-import yargs         from 'yargs';
-import browser       from 'browser-sync';
-import gulp          from 'gulp';
-import panini        from 'panini';
-import rimraf        from 'rimraf';
-import sherpa        from 'style-sherpa';
-import yaml          from 'js-yaml';
-import fs            from 'fs';
+import plugins from 'gulp-load-plugins';
+import yargs from 'yargs';
+import browser from 'browser-sync';
+import gulp from 'gulp';
+import panini from 'panini';
+import rimraf from 'rimraf';
+import sherpa from 'style-sherpa';
+import yaml from 'js-yaml';
+import fs from 'fs';
 import webpackStream from 'webpack-stream';
-import webpack2      from 'webpack';
-import named         from 'vinyl-named';
-import autoprefixer  from 'autoprefixer';
-import imagemin      from 'gulp-imagemin';
+import webpack2 from 'webpack';
+import named from 'vinyl-named';
+import autoprefixer from 'autoprefixer';
+import imagemin from 'gulp-imagemin';
 
-
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass-embedded'));
 const postcss = require('gulp-postcss');
 const uncss = require('postcss-uncss');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -29,7 +30,7 @@ function loadConfig() {
   const unsafe = require('js-yaml-js-types').all;
   const schema = yaml.DEFAULT_SCHEMA.extend(unsafe);
   const ymlFile = fs.readFileSync('config.yml', 'utf8');
-  return yaml.load(ymlFile, {schema});
+  return yaml.load(ymlFile, { schema });
 }
 const { PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
@@ -38,7 +39,7 @@ console.log(UNCSS_OPTIONS);
 // Build the "dist" folder by running all of the below tasks
 // Sass must be run later so UnCSS can search for used classes in the others assets.
 gulp.task('build',
-  gulp.series(clean, gulp.parallel(pages, javascript, images, copy, other), sassBuild, styleGuide)
+  gulp.series(clean, gulp.parallel(pages, javascript, images, copy), sassBuild, styleGuide)
 );
 
 // Build the site, run the server, and watch for file changes
@@ -57,12 +58,6 @@ function clean(done) {
 function copy() {
   return gulp.src(PATHS.assets)
     .pipe(gulp.dest(PATHS.dist + '/assets'));
-}
-
-// Copy files from src/other to project dist(root)
-function other() {
-  return gulp.src(PATHS.other)
-    .pipe(gulp.dest(PATHS.dist));
 }
 
 // Copy page templates into finished HTML files
@@ -104,14 +99,13 @@ function sassBuild() {
   ].filter(Boolean);
 
   return gulp.src('src/assets/scss/app.scss')
-    .pipe($.sourcemaps.init())
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
     .pipe(sass({
       includePaths: PATHS.sass
-    })
-    .on('error', $.sass.logError))
+    }).on('error', sass.logError))
     .pipe(postcss(postCssPlugins))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie11' })))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(PATHS.dist + '/assets/css'))
     .pipe(browser.reload({ stream: true }));
 }
@@ -125,7 +119,7 @@ let webpackConfig = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [ "@babel/preset-env" ],
+            presets: ["@babel/preset-env"],
             compact: false
           }
         }
@@ -154,13 +148,13 @@ function javascript() {
 function images() {
   return gulp.src('src/assets/img/**/*')
     .pipe($.if(PRODUCTION, imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 85, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 85, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
       imagemin.svgo({
         plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
+          { removeViewBox: true },
+          { cleanupIDs: false }
         ]
       })
     ])))
@@ -187,9 +181,8 @@ function watch() {
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/helpers/**/*.js').on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch('src/assets/scss/**/*.*').on('all', sassBuild);
+  gulp.watch('src/assets/scss/**').on('all', sassBuild);
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
   gulp.watch('src/styleguide/**').on('all', gulp.series(styleGuide, browser.reload));
-  gulp.watch('src/other/**/*').on('all', gulp.series(other, browser.reload));
 }
